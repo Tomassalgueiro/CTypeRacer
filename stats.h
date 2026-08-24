6b3a407777f61ed48d199a2721303a7cde2ad889
guinhas <tomasthor8@gmail.com>
2026-08-24 23:06:38 +0100

    created header file

--- stats.h
+++ stats.h
@@ -1,1 +1,20 @@
-struct stats;
+#ifndef STAT_H
+#define STAT_H
+
+#include <stddef.h>
+typedef struct stats Stats;
+
+Stats* stats_create(void);
+void stats_free(Stats *s);
+
+int stats_load(Stats *s, const char *filename);
+int stats_save(const Stats *s, char *filename);
+
+void stats_record_game(Stats *s, double time_elapsed, size_t chars_typed);
+
+double stats_calculate_wpm(double time_elapsed_seconds, size_t chars_typed);
+double stats_get_overall_wpm(const Stats *s);
+double stats_get_best_wpm(const Stats *s);
+int stats_get_games_played(const Stats *s);
+
+#endif
